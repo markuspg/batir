2cb3d1b76281d5dc02ec9147262e13451b5cf074
markuspg <markuspg@users.noreply.github.com>
2021-02-02 22:05:42 +0100

    Extend documentation of the Batir module

--- lib/batir.rb
+++ lib/batir.rb
@@ -8,5 +8,14 @@ require_relative 'batir/version'
 # This module contains the entire functionality of Batir.
 #
 # Some useful helpers are included as methods too.
+#
+# The main components of Batir are:
+#
+# * Command - module defining a basic interface for command objects
+# * RubyCommand - class for treating a block of Ruby code as command
+# * ShellCommand - class allowing to execute shell commands in a platform
+#   independent fashion
+# * CommandSequence - class for grouping and controlling sequences of Commands
+# * Configurator - helper class for creating configuration files as Ruby code
 module Batir
 end
